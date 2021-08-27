0b8c78d1e15c061d43fdd3ef07def7c765b4359c
Mohamed AbdElZaher <hamada6_4@hotmail.com>
2021-08-26 19:21:57 -0500

    Created Model

---
+++ Orders/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orders.Models
+{
+    public class Order
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; private set; }
+        public int CustomerId { get; set; }
+        public string Id { get; private set; }
+        public OrderStatuses Status { get; set; }
+
+        public Order(string name, string description, DateTime created, int customerId, string Id)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Created = created;
+            this.CustomerId = customerId;
+            this.Id = Id;
+            this.Status = OrderStatuses.CREATED;
+        }
+    }
+}
---
+++ Orders/Models/OrderStatuses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orders.Models
+{
+    [Flags]
+    public enum OrderStatuses
+    {
+        CREATED = 2,
+        PROCESSING = 4,
+        COMPLETED = 8,
+        CANCELLED = 16,
+        CLOSED = 32
+    }
+}
